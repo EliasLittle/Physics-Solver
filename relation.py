552ba4acbe9312c37dad7ab12ee0f3b85e534f67
Elias Little <eliasjlittle@gmail.com>
2020-01-06 14:56:29 -0600

    Rewrote solve function and fixed code as needed as a result. Also corrected parseExp function that incorrectly double added properties to the props list

--- relation.py
+++ relation.py
@@ -3,6 +3,7 @@ from property import *
 from Operator import *
 from relatable import *
 import re
+from random import random
 
 """
 A relation between a set of properties
@@ -11,18 +12,16 @@ A child of method
 e.g. {F,m,a} takes two solves for the third
 """
 class Relation:
-    props = []
-    formula = [] # QUESTION: Is this needed, can it be deleted?
 
     def __init__(self,expression,properties=[]):
         self.props = properties #A Collection of property objects
         if len(self.props) != 0:
             for p in self.props:
                 p.addRelation(self)
+        #Precondition: Expression given so that it equals 0
         self.expression = expression #For giving a printable string
-        self.dVar, self.vars = self.parseVars(expression) #Assigns the property placeholder tags that parseVars provides
+        self.vars = self.parseVars(expression) #Assigns the property placeholder tags that parseVars provides
         self.evaluable = ET.constructTree(self.parseExp(expression)) #Creates an ExpressionTree from the postfix that parseExp returns
-
 
     def getProps(self):
         return props
@@ -36,49 +35,70 @@ class Relation:
         return self.getName()
 
 
-    # Parses the expression for the variables, both default and other properties
+    # Parses the expression for the variable
     def parseVars(self,s):
-        s = s.split('=')
-        s1 = s[0][1:-1]
-        s2 = re.findall('\{(.*?)\}',s[1])
-
-        return [s1,s2]
-
+        return re.findall('\{(.*?)\}',s)
 
     def parseExp(self,s):
         inf = []
         # Takes the right side of the equation, and seperates all variables and operators, then splits all into a list
-        s = s.split('=')[1].replace('{','|').replace('}','|')[1:-1].split('|')
+        s = s.replace('{','|').replace('}','|')[1:-1].split('|')
         for i in s:
             if isOperator(i):
                 inf.append(Operator(i))
             else:
-                if len(self.props) != 0:
+                if i in set(prop.tag for prop in self.props):
                     for prop in self.props:
                         if i == prop.tag:
                             inf.append(prop)
                             break
-                newProp = Property(i)
-                newProp.addRelation(self)
-                self.props.append(newProp)
-                inf.append(newProp)
-        #print("--- Infix list created ---")
+                else:
+                    newProp = Property(i)
+                    newProp.addRelation(self)
+                    self.props.append(newProp)
+                    inf.append(newProp)
+        # print("--- Infix list created ---")
+        # print([p.name for p in inf])
 
         return infToPostf(inf)
 
 
     # TODO: Use the assigned formula to solve for the given 'variable'
-    def solve(self, varTag):
-        # Take self.expression, and transform it to solve for the given 'variable'
-        # Evaluate the new expression tree and return the answer
+    def solve(self, var):
+        # Check for other variable's values
         for prop in self.props:
-            if prop.value == None:
-                print("Missing Values")
-                return
-        if varTag == self.dVar:
-            return self.evaluable.solve()
+            if (prop.value == None and prop != var):
+                prop.solve()
+
+        # Evaluate the new expression tree and return the answer
+        def f(x):
+            prop.value = x
+            return ET.evaluateExpressionTree(self.evaluable)
+
+        a = random()*1000
+        b = 0
+        x = a+b/2
+        if(f(a)*f(b) > 0):
+            print(f(a))
+            print(f(b))
+            print("fa and fb must be different signs")
         else:
-            print("Not yet implemented")
+            run = True
+            tries = 0
+            while run and tries < 100:
+                if abs(f(x)) < 0.001:
+                    prop.value = x
+                    run = false
+                    break
+                if f(a)*f(x) > 0:
+                    a=x
+                else:
+                    b=x
+                x=(a+b)/2
+                tries += 1
+
+
+
 
 ##-------------------------------------#
 
@@ -106,7 +126,7 @@ def infToPostf(infix):
             if(len(stack)==0 or top.prec < i.prec or top.value == '('):
                 stack.append(i)
             else:
-                while(top.prec >= i.prec and top.value not in paren):
+                while(top.prec >= i.prec and top.value not in paren and len(stack) > 1):
                     postfix.append(stack.pop())
                     top = stack[len(stack)-1]
                 stack.append(i)
@@ -119,6 +139,8 @@ def infToPostf(infix):
     while len(stack) > 0:
         postfix.append(stack.pop())
 
+    # print("--- Postfix list created ---")
+    # print([p.name for p in postfix])
     return postfix
 
 if __name__ == '__main__':
